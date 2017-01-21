cca971e4564a2d0966d5d15542f92455c727e399
Dmitry <dimk00z@gmail.com>
2017-01-21 17:46:46 +0300

    Удалил лишнее, добавил lower и убрал else

--- lang_frequency.py
+++ lang_frequency.py
@@ -12,7 +12,7 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text, count):
-    words = findall(r'\w+', text)
+    words = findall(r'\w+', text.lower())
     return Counter(words).most_common(count)
 
 
@@ -23,17 +23,20 @@ def read_txt_filenames_from_args():
     return args_parser.parse_args().txt_file_name
 
 
+def print_frequent_words(words):
+    print("Десять самых часто встречающихся слов в тексте:")
+    for word in words:
+        print("Слово: '{}' встречается {} раз".format(word[0],
+                                                      str(word[1])))
+
+
 if __name__ == '__main__':
     txt_file_names = read_txt_filenames_from_args()
     for txt_file_name in txt_file_names:
         text_file = load_data(txt_file_name)
         if not text_file:
-            print("Файла {} не найдено!".format(txt_file_name))
-        else:
-            print("Загружен файл {}".format(txt_file_name))
-            frequent_words = get_most_frequent_words(text_file, 10)
-            print("Десять самых часто встречающихся слов в тексте:")
-            for word in frequent_words:
-                print("Слово: '{}' встречается {} раз".format(word[0],
-                                                              str(word[1])))
+            print("\nФайла {} не найдено!".format(txt_file_name))
+            continue
+        print("\nЗагружен файл {}".format(txt_file_name))
+        print_frequent_words(get_most_frequent_words(text_file, 10))
     print("\nПрограмма завершена.")
